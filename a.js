01a32aa000d0be230a2cca31433bfc05612b6020
Kevin Hua <boogerlad@gmail.com>
2016-01-19 06:44:13 -0500

    new scroll, and switch to svg for down chevron

--- a.js
+++ a.js
@@ -1,21 +1,89 @@
-var foo;
-var j = 0;
+// first add raf shim
+// http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
+window.requestAnimFrame =
+(
+	function()
+	{
+		return  window.requestAnimationFrame ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame    ||
+			function(callback)
+			{
+				window.setTimeout(callback, 1000 / 60);
+			};
+	}
+)();
+
+// main function
+function scrollToY(scrollTargetY, speed, easing)
+{
+	// scrollTargetY: the target scrollY property of the window
+	// speed: time in pixels per second
+	// easing: easing equation to use
+
+	var scrollY = window.scrollY;
+	var scrollTargetY = scrollTargetY || 0;
+	var speed = speed || 2000;
+	var easing = easing || 'easeOutSine';
+	var currentTime = 0;
+
+	// min time .1, max time .8 seconds
+	var time = Math.max(.1, Math.min(Math.abs(scrollY - scrollTargetY) / speed, .8));
+
+	// easing equations from https://github.com/danro/easing-js/blob/master/easing.js
+	var PI_D2 = Math.PI / 2;
+	var easingEquations =
+	{
+		easeOutSine: function(pos)
+		{
+			return Math.sin(pos * (Math.PI / 2));
+		},
+		easeInOutSine: function(pos)
+		{
+			return (-0.5 * (Math.cos(Math.PI * pos) - 1));
+		},
+		easeInOutQuint: function(pos)
+		{
+			if ((pos /= 0.5) < 1)
+			{
+				return 0.5 * Math.pow(pos, 5);
+			}
+			else
+			{
+				return 0.5 * (Math.pow((pos - 2), 5) + 2);
+			}
+		}
+	};
+
+	// add animation loop
+	function tick()
+	{
+		currentTime += 1 / 60;
+
+		var p = currentTime / time;
+		var t = easingEquations[easing](p);
+
+		if (p < 1)
+		{
+			requestAnimFrame(tick);
+			window.scrollTo(0, scrollY + ((scrollTargetY - scrollY) * t));
+		}
+		else
+		{
+			console.log('scroll done');
+			window.scrollTo(0, scrollTargetY);
+		}
+	}
+	// call it once to get started
+	tick();
+}
+
+// scroll it!
 document.getElementById('scroll').onclick = function()
 {
-	foo = setInterval
-	(
-		function()
-		{
-			window.scroll(0, j += 10);
-			if(j >= Math.max(document.documentElement.clientHeight, window.innerHeight || 0))
-			{
-				j = 0;
-				clearInterval(foo);
-			}
-		},
-		1
-	);
-};
+	scrollToY(Math.max(document.documentElement.clientHeight, window.innerHeight || 0), 600, 'easeInOutQuint');
+}
+
 particlesJS.load('particle', 'particles.json');
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
